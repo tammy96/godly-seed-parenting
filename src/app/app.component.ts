f2e39f28b1c1aaa391f12c38f900dfcb89bd0c1c
Collins Thomas <thomascollins582@gmail.com>
2020-05-23 03:00:24 +0100

    Minor Style Tweak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { AngularFireFunctions } from "@angular/fire/functions";
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { UsersService } from './services/users.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { IUser } from './interface/iUser';
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-root',
@@ -9,9 +13,12 @@ import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 })
 export class AppComponent {
   title = 'godly-seed-parenting';
+  user: IUser;
+  faUser = faUser;
   public showSideMenu: boolean;
   public showFiller: boolean;
-  constructor(public breakpointObserver: BreakpointObserver) {}
+  constructor(public breakpointObserver: BreakpointObserver,
+     private userService: UsersService, private afAuth: AngularFireAuth) {}
 
   ngOnInit(): void {
     this.breakpointObserver
@@ -23,5 +30,17 @@ export class AppComponent {
           this.showSideMenu = false;
         }
       });
+      this.afAuth.authState.subscribe(value => {
+        if (value.uid) {
+          this.userService.getUser(value.uid).subscribe(userValue => {
+            this.user = userValue;
+          })
+        }
+      })
+  };
+
+  logout() {
+    this.afAuth.signOut();
   }
+
 }
