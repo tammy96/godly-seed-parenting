647952d3fc39798825b55a306b9f499e3d455873
Collins Thomas <thomascollins582@gmail.com>
2020-05-27 23:37:54 -0700

    Added Pasword reset but not tested yet

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class ForgotPasswordComponent implements OnInit {
 
   value: string = '';
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
   ngOnInit(): void {
+  }
+
+  sendPasswordReset() {
+    let actionCodeSettings = {
+      // Change URL to domain that will be bought later,
+
+      url: 'https://godly-seed-parenting-bf240.firebaseapp.com'
+    }
+
+    this.afAuth.sendPasswordResetEmail(this.value, actionCodeSettings)
   }
 
 }
